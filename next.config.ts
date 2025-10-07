90af517f89818e02472e9c564d87accddeedeb77
MandavkarPranjal <programmer.pranjal@gmail.com>
2025-10-07 10:09:11 +0530

    added mail links

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,16 @@ const nextConfig: NextConfig = {
                 destination: 'https://x.com/pr5dev',
                 permanent: true
             },
+            {
+                source: '/mail',
+                destination: 'https://mail.google.com/mail/?view=cm&fs=1&to=contact@pr5.dev&su=connect&body=hi%20pranjal%2C%20I%20saw%20your%20work%20bro%2C%20thanks%20for%20contributing%20lol',
+                permanent: true
+            },
+            {
+                source: '/0',
+                destination: 'https://0.email/mail/compose?to=contact@pr5.dev&subject=connect&body=hi%20pranjal%2C%20I%20saw%20your%20work%20bro%2C%20thanks%20for%20contributing%20lol',
+                permanent: true
+            },
         ]
     }
 };

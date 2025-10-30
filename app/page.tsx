1870a0226bb8b3efd52de4874f8d25b61e43ebe7
MandavkarPranjal <programmer.pranjal@gmail.com>
2025-10-30 23:40:40 +0530

    added projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { GithubInfo } from "@/components/github-info"
 import Link from "next/link"
 
 export default function Home() {
@@ -19,27 +20,58 @@ export default function Home() {
                     </p>
                     <p>
                         also a big fan of open source, OSS,&nbsp;
-                        <Link href="https://www.youtube.com/@t3dotgg" className="underline hover:text-foreground/85 cursor-pointer">
+                        <Link href="https://www.youtube.com/@t3dotgg" className="underline underline-offset-4 hover:text-foreground/85 cursor-pointer">
                             theo
                         </Link>, and&nbsp;
-                        <Link href="https://x.com" className="underline hover:text-foreground/85 cursor-pointer">
+                        <Link href="https://x.com" className="underline underline-offset-4 hover:text-foreground/85 cursor-pointer">
                             X(formerly twitter)
                         </Link> as platform
                     </p>
                     <p>
                         if want to know more about me,&nbsp;
-                        <Link href="blog/things-i-believe" className="underline text-foreground/85 cursor-pointer">
+                        <Link href="blog/things-i-believe" className="underline underline-offset-4 text-foreground/85 cursor-pointer">
                             here are something i believe in
                         </Link> and&nbsp;
-                        <Link href="/blog/these-are-bullshits" className="underline text-foreground/85 cursor-pointer">
+                        <Link href="/blog/these-are-bullshits" className="underline underline-offset-4 text-foreground/85 cursor-pointer">
                             some that i don't
                         </Link>
                     </p>
                 </div>
 
-                <h1 className="mt-8 mb-6 text-xl font-serif font-medium tracking-tight text-foreground/95 italic">
-                    some cool projects
+                <h1 className="mt-8 mb-6 text-xl font-serif font-medium tracking-tight text-foreground italic">
+                    some cool projects :)
                 </h1>
+                <div className="space-y-6 text-base leading-relaxed text-foreground/60">
+                    <div className="flex flex-col space-y-2 tracking-tight">
+                        <Link
+                            href="https://whataduck.pr5.dev"
+                            className="text-lg font-medium hover:text-foreground/85 cursor-pointer"
+                            target="_blank"
+                        >
+                            <span className="underline underline-offset-4">WhataDuck</span> - A Simple &amp; Lightning Fast Search Engine with bangs
+                        </Link>
+                        <GithubInfo
+                            owner="mandavkarpranjal"
+                            repo="whataduck"
+                            className="hover:text-foreground/85 cursor-pointer"
+                        />
+                    </div>
+
+                    <div className="flex flex-col space-y-2 tracking-tight">
+                        <Link
+                            href="https://www.npmjs.com/package/opengraph-cli"
+                            className="text-lg font-medium hover:text-foreground/85 cursor-pointer"
+                            target="_blank"
+                        >
+                            <span className="underline underline-offset-4">Opengraph-CLI</span> - Get Opengraph metadata &amp; image for your websites (even local dev server), right inside of terminal
+                        </Link>
+                        <GithubInfo
+                            owner="mandavkarpranjal"
+                            repo="opengraph-cli"
+                            className="hover:text-foreground/85 cursor-pointer"
+                        />
+                    </div>
+                </div>
             </div>
         </main >
     )

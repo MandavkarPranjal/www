d7bfb348a7d7d366a1df5f761c1bb939f4e29cbe
MandavkarPranjal <programmer.pranjal@gmail.com>
2026-01-18 21:26:29 +0530

    add: new project

--- lib/projects.ts
+++ lib/projects.ts
@@ -15,6 +15,14 @@ export const projects: Project[] = [
         repo: "whataduck",
     },
     {
+        title: "OpenCode Notifier",
+        description:
+            "Opencode Plugin that sends system notification when permission is needed, generation is complete, error occurs or question tool is invoked",
+        href: "https://www.npmjs.com/package/@pranjalmandavkar/opencode-notifier",
+        owner: "mandavkarpranjal",
+        repo: "opencode-notifier",
+    },
+    {
         title: "Opengraph-CLI",
         description:
             "Get Opengraph metadata & image for your websites (even local dev server), right inside of terminal",
